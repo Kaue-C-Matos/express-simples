2e3df00a102fca6f12500892273e6f49fd868c5a
Kaue-C-Matos <129345524+Kaue-C-Matos@users.noreply.github.com>
2023-05-10 21:06:00 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
 const express = require("express");
 const server = express();
-const router = express.Router();
+server.use(express.json());
 const port = 3000; //N° da porta
-
-server.use(router);
 
 server.listen(port, ()=>{
     console.log(`server rodando na porta ${port}`)
 })
 
-router.get("/health", (req, res)=>{
-    res.json("server is running")
+let tasks = [
+    {
+        id: 1, 
+        nome:"comprar leite", 
+        descricao: "ir até a esquina e comprar leite", 
+        isDone: false
+    },
+    {
+        id: 2, 
+        nome:"lavar as janelas", 
+        descricao: "lavar as janelas da cozinha e dos quartos", 
+        isDone: true
+    }
+]
+
+server.get("/tasks", (req, res)=>{
+    res.json({tasks})
+})
+
+server.post("/tasks", (req, res)=>{
+    console.log(req.body)
+    const newTask={
+        id: tasks.length + 1,
+        nome: req.body.nome,
+        descricao: req.body.descricao,
+        isDone: req.body.isDone
+    }
+    tasks.push(newTask)
+    res.json({
+        tasks: newTask
+    })
+})
+
+server.put("/tasks/:id", (req, res)=>{
+    const id = Number(req.params.id);
+    const task = tasks.find((task)=>{
+        return task.id === id
+    })
+    task.nome = req.body.nome
+    task.descricao = req.body.descricao
+    task.isDone = req.body.isDone
+    res.json({task})
+})
+
+server.delete("/tasks/:id", (req, res)=>{
+    const id = Number(req.params.id);
+    tasks = tasks.filter(task=>{
+        return task.id !== id
+    })
+    res.status(204).send();
 })
